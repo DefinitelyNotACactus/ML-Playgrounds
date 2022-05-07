2bd3d9ef0100603225fe76e3d229355830625205
DefinitelyNotACactus <galvao2010@icloud.com>
2022-05-07 10:09:22 -0300

    KNN viewer

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from dash import Dash, dcc, html, Input, Output, State, no_update, callback_context
 from sklearn.datasets import make_blobs, make_circles, make_moons
 from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from SVM_Parameters import SVM_Parameters
@@ -20,15 +21,23 @@ app = Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 
 app.layout = html.Div([
-    dcc.Store(id="seed", storage_type="memory", data="42"),
+    # Componentes de memória
+    dcc.Store(id="seed-memory", storage_type="memory", data="42"),
+    dcc.Store(id="data-type-memory", storage_type="memory", data="moons"),
+    dcc.Store(id="n-memory", storage_type="memory", data="100"),
+    dcc.Store(id="std-memory", storage_type="memory", data="0.1"),
+    dcc.Store(id="bt-memory", storage_type="memory", data="bt-svm"),
+    # Cabeçalho
     html.Header(id="Header", children=[
         html.H1("Teste como o conjunto de dados ou os parâmetros podem influenciar o modelo de aprendizagem de máquina"),
     ]),
+    # Seletor de algoritmo
     html.Div(id="models", children=[
         html.Button("Nearest Neighbors", id="bt-knn", className="basic-button"),
         html.Button("Support Vector Machine", id="bt-svm", className="basic-button"),
         html.Button("Neural Network", id="bt-nn", className="basic-button", style={"margin-right": "30px"})
     ], className="container-flex"),
+    # Corpo do app
     html.Div(id="main", children=[
         html.Div(children=[
             html.H4("Dados"),
@@ -39,13 +48,13 @@ app.layout = html.Div([
                     {"label": key.upper(), "value": key}
                     for key in ["blobs", "moons", "circles"]
                 ],
-                value="blobs",
+                value="moons",
                 clearable=False,
                 className="dropdown",
             ),
             html.P("Quantidade de pontos"),
             dcc.Input(value=100, id="n-input"),
-            html.P("Ruído dos dados"),
+            html.P("Ruído dos dados", id="std-info"),
             dcc.Input(value=0.1, id="std-input"),
             #html.P("% dos pontos pertencentes à classe amarela"),
             #dcc.Slider(0, 100, value=50, marks=None, tooltip={"placement": "bottom", "always_visible": True}, className="sliders", id="balance-slider"),
@@ -66,8 +75,33 @@ app.layout = html.Div([
             ),
             html.P("Parâmetro de regularização (C)"),
             dcc.Input(value=1.0, id="c-input"),
-            html.Button("Treinar modelo", id="bt-fit", className="basic-button", style={"margin-right": "0px", "margin-left": "0px", "margin-top": "12px"})
-        ], className="column", style={"width": "15%", "position": "relative"}),
+            html.P("Grau (apenas para kernel poly)", id="degree-info"),
+            dcc.Input(value=3, id="degree-input"),
+            html.P("Gama (apenas para kernel rbf, poly, ou sigmoid)", id="gamma-info"),
+            html.P("Use scale, auto, ou um float qualquer"),
+            dcc.Input(value="scale", id="gamma-input"),
+            html.Button("Treinar modelo", id="bt-fit-svm", className="basic-button", style={"margin-right": "0px", "margin-left": "0px", "margin-top": "12px"})
+        ], id="parameters-svm-column", className="column", style={"width": "15%", "position": "relative"}),
+        html.Div(children=[
+            html.H4("Parâmetros"),
+            html.P("Número de vizinhos"),
+            dcc.Input(value=5, id="k-input"),
+            html.P("Pesos"),
+            dcc.Dropdown(
+                id="weights-dropdown",
+                options=[
+                    {"label": key.upper(), "value": key}
+                    for key in ["uniform", "distance"]
+                ],
+                value="uniform",
+                clearable=False,
+                className="dropdown",
+            ),
+            html.P("P"),
+            html.P("P=1 equivale a distância de Manhattan, P=2 Euclidiana, e para outro P qualquer é usada Minkowski"),
+            dcc.Input(value=2, id="p-input"),
+            html.Button("Treinar modelo", id="bt-fit-knn", className="basic-button", style={"margin-right": "0px", "margin-left": "0px", "margin-top": "12px"})
+        ], id="parameters-knn-column", className="column", style={"width": "15%", "position": "relative", "display": "none"}),
         html.Div(children=[
             html.H4("Modelo"),
             #html.P("Acurácia no treino:"),
@@ -93,83 +127,157 @@ buttons = ["bt-knn", "bt-svm", "bt-nn"]
 def set_active(*args):
     ctx = callback_context
     
-    if not ctx.triggered or not any(args):
-        return [
-            "basic-button" if x != 1 else "basic-button selected" for x in range(3)
-        ]
+    if not ctx.triggered or not any(args): return ["basic-button" if x != 1 else "basic-button selected" for x in range(3)]
 
     button_id = ctx.triggered[0]["prop_id"].split(".")[0]
     styles = []
     for button in buttons:
-        if button == button_id:
-            styles.append("basic-button selected")
-        else:
-            styles.append("basic-button")
+        if button == button_id: styles.append("basic-button selected")
+        else: styles.append("basic-button")
 
     return styles
 
+# Callback do clique no botão de gerar dados ou treinar modelo
 @app.callback(
     [
         Output("scatter", "figure"),
         Output("model-accuracy-display", "children"),
-        Output("seed", "data")
+        Output("seed-memory", "data"),
+        Output("data-type-memory", "data"),
+        Output("n-memory", "data"),
+        Output("std-memory", "data"),
     ],
     [
         Input("bt-generate-data", "n_clicks"),
-        Input("bt-fit", "n_clicks"),
-        Input("seed", "data")
+        Input("bt-fit-svm", "n_clicks"),
+        Input("bt-fit-knn", "n_clicks"),
     ],
     [
-        State("n-input", "value"),
-        State("std-input", "value"),
-        #State("balance-slider", "value"),
+        # Memoria parâmetros dados
+        State("seed-memory", "data"),
+        State("data-type-memory", "data"),
+        State("n-memory", "data"),
+        State("std-memory", "data"),
+        # Parâmetros do SVM
         State("kernel-dropdown", "value"),
         State("c-input", "value"),
-        State("data-type-dropdown", "value")
+        State("gamma-input", "value"),
+        State("degree-input", "value"),
+        # Parâmetros do KNN
+        State("k-input", "value"),
+        State("weights-dropdown", "value"),
+        State("p-input", "value"),
+        # Estado do input de dados
+        State("n-input", "value"),
+        State("std-input", "value"),
+        State("data-type-dropdown", "value"),
     ]
 )
-def generate_fit_data(bt_generate_data, bt_fit_data, seed, n, std, kernel, c, data_type):
-    # Obter o id do botão selecionado
+def generate_fit_data(bt_generate, bt_fit_svm, bt_fit_knn, mem_seed, mem_data_type, mem_n, mem_std, kernel, c_input, gamma_input, degree_input, k_input, knn_weights, p_input, n_input, std_input, data_type_input):
+    seed = randint(0, 1000)
+    # Definir se o callback foi ativado por um botão
     ctx = callback_context
+    # Obter o id do botão selecionado (ou não, caso não tenha sido um que disparou o callback)
     button_id = ctx.triggered[0]["prop_id"].split(".")[0]
-    if button_id == "bt-generate-data": seed = randint(0, 1000)
-    # Gerar o dataset
-    if data_type == "blobs": X, y = make_blobs(n_samples=int(n), centers=2, n_features=2, cluster_std=float(std)*10, random_state=int(seed))
-    elif data_type == "circles": X, y = make_circles(n_samples=int(n), noise=float(std), random_state=int(seed))
-    elif data_type == "moons": X, y = make_moons(n_samples=int(n), noise=float(std), random_state=int(seed))
+    if button_id == "bt-generate-data": X, y = get_dataset(data_type_input, n_input, std_input, seed)
+    else: X, y = get_dataset(mem_data_type, mem_n, mem_std, mem_seed)
     # Gerar o gráfico de saída
+    fig = generate_figure(X)
+    # Gerar o modelo
+    if button_id == "bt-fit-svm":
+        if gamma_input != "auto" and gamma_input != "scale": gamma_input = float(gamma_input)
+        model = SVC(kernel=kernel, C=float(c_input), gamma=gamma_input, degree=float(degree_input))
+        model.fit(X, y)
+        fig.add_trace(plot_svc_decision_function((min(X[:, 0]), max(X[:, 0])), (min(X[:, 1]), max(X[:, 1])), model))
+    if button_id == "bt-fit-knn":
+        model =  KNeighborsClassifier(n_neighbors=int(k_input), weights=knn_weights, p=int(p_input))
+        model.fit(X, y)
+        fig.add_trace(plot_knn_decision_function((min(X[:, 0]), max(X[:, 0])), (min(X[:, 1]), max(X[:, 1])), model))
+    # Adicionar os pontos do dataset ao gráfico
+    fig.add_trace(
+        go.Scatter(x=X[:, 0], y=X[:, 1], mode="markers", marker=dict(color=y, colorscale=colorscale), showlegend=False, name="")
+    )
+    if button_id == "bt-fit-svm" or button_id == "bt-fit-knn": return fig, "Acurácia do modelo: {0:.2f}%".format(accuracy_score(y, model.predict(X)) * 100), no_update, no_update, no_update, no_update
+    return fig, "Clique em treinar modelo para ver o desempenho dele", str(seed), str(data_type_input), int(n_input), float(std_input)
+    
+@app.callback(Output("std-info", "children"), Input("data-type-dropdown", "value"))
+def update_std_info(data_type):
+    if data_type == "blobs": return "Desvio padrão das classes"
+    return "Ruído dos dados"
+    
+@app.callback([Output("gamma-info", "children"), Output("degree-info", "children")], Input("kernel-dropdown", "value"))
+def update_svm_parameters_info(kernel_type):
+    if kernel_type == "linear": return "Gama (não aplicável)", "Grau (não aplicável)"
+    if kernel_type == "poly": return "Gama", "Grau"
+    return "Gama", "Grau (não aplicável)"
+
+@app.callback(
+    [
+        Output("parameters-svm-column", "style"),
+        Output("parameters-knn-column", "style"),
+        Output("bt-memory", "data"),
+    ],
+    [
+        Input("bt-knn", "n_clicks"),
+        Input("bt-svm", "n_clicks"),
+        Input("bt-nn", "n_clicks"),
+        Input("bt-memory", "data"),
+    ],
+)
+def update_parameters(bt_knn, bt_svm, bt_nn, bt_memory):
+    # Definir se o callback foi ativado por um botão
+    ctx = callback_context
+    # Caso entre no if abaixo é por ter sido o startup do aplicativo
+    if not ctx.triggered or not any([bt_knn, bt_svm, bt_nn, bt_memory]): return {"width": "15%", "position": "relative"}, {"width": "15%", "position": "relative", "display": "none"}, no_update
+    # Obter o id do botão selecionado (ou não, caso não tenha sido um que disparou o callback)
+    button_id = ctx.triggered[0]["prop_id"].split(".")[0]
+    # Foi clicado no mesmo botão
+    if button_id == bt_memory: return no_update, no_update, no_update
+    if button_id == "bt-svm": return {"width": "15%", "position": "relative"}, {"width": "15%", "position": "relative", "display": "none"}, "bt-svm"
+    if button_id == "bt-knn": return {"width": "15%", "position": "relative", "display": "none"}, {"width": "15%", "position": "relative"}, "bt-knn"
+    return no_update, no_update, no_update
+    
+# Função para gerar uma figura seguindo os padrões do dashboard
+def generate_figure(X):
     fig = go.Figure()
     fig.update_layout(showlegend=False, margin=dict(l=16, r=16, t=16, b=16), xaxis=dict(showgrid=False, zeroline=False), yaxis=dict(showgrid=False, zeroline=False), plot_bgcolor="rgba(203, 203, 212, .3)",
         paper_bgcolor="rgba(0,0,0,0)",
         font_family="DIN Alternate",
         xaxis_range=[X[:, 0].min() - 1, X[:, 0].max() + 1],
         yaxis_range=[X[:, 1].min() - 1, X[:, 1].max() + 1])
-    if button_id == "bt-fit": # Clique em bt-fit
-        model = SVC(kernel=kernel, C=c)
-        model.fit(X, y)
-        fig.add_trace(plot_svc_decision_function((min(X[:, 0]), max(X[:, 0])), (min(X[:, 1]), max(X[:, 1])), model))
-    # Adicionar os pontos
-    fig.add_trace(
-        go.Scatter(x=X[:, 0], y=X[:, 1], mode="markers", marker=dict(color=y, colorscale=colorscale), showlegend=False, name="")
-    )
-    if button_id == "bt-fit": return fig, "Acurácia do modelo: {0:.2f}".format(accuracy_score(y, model.predict(X))), no_update
-    return fig, "Clique em treinar modelo para ver o desempenho dele", str(seed)
     
-def plot_svc_decision_function(xlim, ylim, model, margin=1):
-    # create grid to evaluate model
-    x = np.linspace(xlim[0] - margin, xlim[1] + margin, 100)
-    y = np.linspace(ylim[0] - margin, ylim[1] + margin, 100)
+    return fig
+    
+# Função para gerar o dataset
+def get_dataset(data_type, n, std, seed):
+    if data_type == "blobs": X, y = make_blobs(n_samples=int(n), centers=2, n_features=2, cluster_std=float(std), random_state=int(seed))
+    elif data_type == "circles": X, y = make_circles(n_samples=int(n), noise=float(std), random_state=int(seed))
+    elif data_type == "moons": X, y = make_moons(n_samples=int(n), noise=float(std), random_state=int(seed))
+    
+    return X, y
+    
+def plot_svc_decision_function(xlim, ylim, model, margin=1, size=100):
+    x = np.linspace(xlim[0] - margin, xlim[1] + margin, size)
+    y = np.linspace(ylim[0] - margin, ylim[1] + margin, size)
     xx, yy = np.meshgrid(x, y)
     xy = np.vstack([xx.ravel(), yy.ravel()]).T
     Z = model.decision_function(xy).reshape(xx.shape)
     # plot decision boundary and margins
     return go.Contour(x=x, y=y, z=Z, colorscale=colorscale,
-            contours_coloring="heatmap", hoverinfo="none", showlegend=False, showscale=False
+            contours_coloring="heatmap", hoverinfo="none", showlegend=False, showscale=False, opacity=0.8
             #line_width=2,
         )
-    #plt.contour(X, Y, P,  colors=color,
-    #           levels=[-1, 0, 1], alpha=0.5,
-    #           linestyles=['--', '-', '--'])
                
+def plot_knn_decision_function(xlim, ylim, model, margin=1, size=100):
+    x = np.linspace(xlim[0] - margin, xlim[1] + margin, size)
+    y = np.linspace(ylim[0] - margin, ylim[1] + margin, size)
+    xx, yy = np.meshgrid(x, y)
+    Z = model.predict_proba(np.c_[xx.ravel(), yy.ravel()])[:, 1]
+    Z = Z.reshape(xx.shape)
+    return go.Contour(x=x, y=y, z=Z, colorscale=colorscale,
+            contours_coloring="heatmap", hoverinfo="none", showlegend=False, showscale=False, opacity=0.8
+            #line_width=2,
+    )
+    
 if __name__ == '__main__':
     app.run_server(debug=True)
